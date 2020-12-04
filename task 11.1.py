ad66a0f472a33ac79543bb2b3cba9343c208ec18
Roman Shirenkov <shirenkov_roman@mail.ru>
2020-12-05 01:49:22 +0300

    corrected

--- task 11.1.py
+++ task 11.1.py
@@ -4,60 +4,57 @@
 import requests
 
 
-class VkUsers:
+class User:
     url = "https://api.vk.com/method/"
 
-    def __init__(self, token, version, user_ids=None):
-        self.user_ids = user_ids
-        self.token = token
-        self.version = version
+    def __init__(self, userid, token_user, version_user):
+        self.user_ids = userid
+        self.token = token_user
+        self.version = version_user
         self.params = {
             "user_ids": self.user_ids,
             "access_token": self.token,
             "v": self.version,
             "fields": "domain"
             }
-        self.basic_fields = requests.get(self.url + "users.get", self.params).json()["response"][0]
-        
-    # Список друзей
-    def friends_get(self):
-        if "deactivated" not in self.basic_fields:
-            url_friends_get = self.url + "friends.get"
-            friends_params = {"user_id": self.user_ids, "access_token": self.token, "v": self.version}
-            friends_get = requests.get(url_friends_get, friends_params).json()["response"]["items"]
-            return set(friends_get)
-        else:
-            return set()
+        self.basic_fields = requests.get(self.url + "users.get", self.params).json()
+        self.friends = requests.get(self.url + "friends.get", {
+            "user_id": self.user_ids,
+            "access_token": self.token,
+            "v": self.version
+            }).json()
 
+    def __and__(self, otheruser):
+        self.mutaluserlist = []
+        for i in self.friends["response"]["items"]:
+            if i in otheruser.friends["response"]["items"]:
+                self.mutaluserlist.append(User(i, self.token, self.token))
+        return self.mutaluserlist
+        
     def __str__(self):
-        return "https://vk.com/" + self.basic_fields["domain"]
+        return "https://vk.com/" + self.basic_fields["response"][0]["domain"]
     
         
 # Задача №1
 # Пользователя нужно описать с помощью класса и реализовать метод поиска общих друзей, используя API VK.
 
 token = "10b2e6b1a90a01875cfaa0d2dd307b7a73a15ceb1acf0c0f2a9e9c586f3b597815652e5c28ed8a1baf13c"
+version = "5.126"
 
-user = VkUsers(token, "5.126")
-print(user.basic_fields)
-print()
+userid_1 = None  # 552934290 # 'count': 16
+userid_2 = "551843217"  # 171691064
+
+user_1 = User(userid_1, token, version)
+user_2 = User(userid_2, token, version)
 
 # Задача №2 
 # Поиск общих друзей должен происходить с помощью оператора &, т.е. user1 & user2 должен выдать список общих друзей
 # пользователей user1 и user2, в этом списке должны быть экземпляры классов.
 
-user1 = user.friends_get()
-user2 = VkUsers(token, "5.126", "551843217").friends_get()
+mutaluserlist = user_1 & user_2
+print(mutaluserlist)
 
-# список общих друзей пользователей user1 и user2
-list_frends = []
-for i in (user1 & user2):
-    specimen = VkUsers(token, "5.126", i)
-    list_frends.append(specimen)
-    
-print(list_frends)
-print()
 
 # Задача №3
 # Вывод print(user) должен выводить ссылку на профиль пользователя в сети VK
-print(user)
+print(user_1)  
